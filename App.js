61e19e622903cb9d0d8d6e995b908dfeb83c03bd
AmmonMatthews <ammonmatthews@gmail.com>
2020-12-20 16:51:00 -0700

    adding navigation buttons

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 
-function HomeScreen() {
+function HomeScreen({navigation}) {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>This is the main screen for App</Text>
+      <Button
+        title="Go to details"
+        onPress={() => navigation.navigate('Details')}
+      />
+
     </View>
   )
 };
 
-function DetailsScreen() {
+function DetailsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
     </View>
   );
 }
